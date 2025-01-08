e1cd61e0b48e0d3d09d6b09291fe4d42a9098ff2
difrancesco1 <joshiqua@gmail.com>
2025-01-08 02:49:19 -0800

    added navigation to 'View projects' button

--- portfolio/src/components/HeroBtnContainter.js
+++ portfolio/src/components/HeroBtnContainter.js
@@ -4,7 +4,7 @@ import '../App.css'
 const HeroBtnContainter = () => {
     return (
         <div className="hero-btn-cont">
-            <button href="#" className="projects-btn">VIEW PROJECTS <span className="projects-btn-span"></span> </button>
+            <a href="#my-work"><button className="projects-btn">VIEW PROJECTS <span className="projects-btn-span"></span></button></a>
             <a target="_blank" rel="noopener noreferrer" href='/SoftwareEngineerResume.pdf' className="resume-a"><button className="resume-btn"><p className="hero-p-btn">VIEW RESUME</p><span className="hero-span"></span></button></a>
         </div>
     )
